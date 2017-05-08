528fcdc4a44f4009ebb282c78b9f596076c8a989
w1x <w1x@w1x>
2017-05-08 17:35:35 -0400

    new commit

--- PHPClass2017/week3/lab/Registration/templates/Admin.php
+++ PHPClass2017/week3/lab/Registration/templates/Admin.php
@@ -4,15 +4,20 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+ <?php
+        require 'access-required.php';
+        ?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     <h1>Welcome to Admin Page</h1>
+    
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <h2>User ID : <?php $user = $_SESSION['user_id'];
+        echo "$user"; ?></h2>
+        <a href ='../logout.php'> Logout </a>
+       
     </body>
 </html>
